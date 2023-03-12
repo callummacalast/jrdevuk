23f67c2bd50c0af6a4f5f7e2c6130bdebbfaa8ea
Callum Macalast <calmacalast@gmail.com>
2023-03-12 22:50:46 +0000

    Initial setup / commit

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'edit articles']);
+        Permission::create(['name' => 'delete articles']);
+        Permission::create(['name' => 'publish articles']);
+        Permission::create(['name' => 'unpublish articles']);
+
+        // create roles and assign created permissions
+
+        // this can be done as separate statements
+        $role1 = Role::create(['name' => 'user']);
+        $role1->givePermissionTo('edit articles');
+
+        // or may be done by chaining
+        $role2 = Role::create(['name' => 'admin']);
+        $role2->givePermissionTo(['publish articles', 'unpublish articles']);
+
+        $role3 = Role::create(['name' => 'super-admin']);
+        $role3->givePermissionTo(Permission::all());
+
+
+        // create demo users
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Example User',
+            'email' => 'test@example.com',
+        ]);
+        $user->assignRole($role1);
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Example Admin',
+            'email' => 'admin@example.com',
+        ]);
+        $user->assignRole($role2);
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Example Super-Admin User',
+            'email' => 'superadmin@example.com',
+        ]);
+        $user->assignRole($role3);
+    }
+}
